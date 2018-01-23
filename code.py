0e9219702c0c409f27c9ebf2b4bb4b86fa9fc6b5
jdb5523 <jareddb511@gmail.com>
2018-01-22 16:00:01 -0800

    using biopython lib

--- code.py
+++ code.py
@@ -1,4 +1,17 @@
-from Bio.Seq import Seq
+from Bio import SeqIO
+from decimal import *
 
-#create a sequence object
-my_seq = Seq('CATGTAGACTAG')
+seqid = ""
+seq = ""
+maxcount = 0
+for seq_record in SeqIO.parse("data.txt", "fasta"):
+    var = str(seq_record.seq).count("C")
+    var += str(seq_record.seq).count("G")
+    if var > maxcount:
+        maxcount = var / len(str(seq_record.seq))
+        seqid = seq_record.id
+        seq = seq_record.seq
+
+maxcount = float(maxcount * 100)
+format(maxcount, '.2f')
+print(seqid + "\n" + str(maxcount))
